03daba35ad361cf541e1a3885fde517cd28292c5
RUEI-CHENG HUANG <RichardHuang@richardde-imac-2.local>
2019-07-30 22:35:11 +0800

    Initial Commit

---
+++ ColorFinderApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColorFinderApp
+//
+//  Created by Richard on 2019/7/30.
+//  Copyright © 2019 Richard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
